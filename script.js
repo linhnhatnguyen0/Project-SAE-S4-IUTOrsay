098ea974503ff637160e15e2242aaa01ae0355e7
lusjedji <102389353+lusjedji@users.noreply.github.com>
2023-04-15 18:35:46 +0200

    ajout marker moissan

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var map = tt.map({
 map.setZoom(17);
 map.setCenter([2.170936, 48.711115]);
 
+//cafet IUT
 var divElement = document.createElement("div");
 divElement.innerHTML =
   '<div class="marker"><img src="./img/restaurant-icon.png" width="32" height="40"><p>Cafeteria Amphi Hubert Coudane</p></div>';
@@ -12,6 +13,29 @@ var marker = new tt.Marker({
   element: divElement,
 })
   .setLngLat([2.170191469892004, 48.710711586415286])
+  .addTo(map)
+  .setPopup(new tt.Popup().setHTML("<b>Hello world!</b>"));
+
+//cafet moissan
+var divElement2 = document.createElement("div");
+divElement2.innerHTML =
+  '<div class="marker"><img src="./img/restaurant-icon.png" width="32" height="40"><p>Cafeteria Henri Moissan</p></div>';
+var marker2 = new tt.Marker({
+  element: divElement2,
+})
+  .setLngLat([2.1723943695285683,48.711530079110744])
+  .addTo(map)
+  .setPopup(new tt.Popup().setHTML("<b>Hello world!</b>"));
+
+
+//restaurant universitaire moissan
+var divElement2 = document.createElement("div");
+divElement2.innerHTML =
+  '<div class="marker"><img src="./img/restaurant-icon.png" width="32" height="40"><p>Restaurant universitaire Henri Moissan</p></div>';
+var marker2 = new tt.Marker({
+  element: divElement2,
+})
+  .setLngLat([2.172448013711649, 48.71101681800649])
   .addTo(map)
   .setPopup(new tt.Popup().setHTML("<b>Hello world!</b>"));
 
