f5c731a4dc6adfa1c36386076a16c0e39f75b349
lusjedji <102389353+lusjedji@users.noreply.github.com>
2023-04-17 15:31:20 +0200

    ajout classe Marker et ajout de la methode addMarker

--- script.js
+++ script.js
@@ -5,18 +5,44 @@ var map = tt.map({
 map.setZoom(17);
 map.setCenter([2.170936, 48.711115]);
 
-//cafet IUT
-var divElement = document.createElement("div");
-divElement.innerHTML =
-  '<div class="marker"><img src="./img/restaurant-icon.png" width="32" height="40"><p>Cafeteria Amphi Hubert Coudane</p></div>';
-var marker = new tt.Marker({
-  element: divElement,
-})
-  .setLngLat([2.170191469892004, 48.710711586415286])
-  .addTo(map)
-  .setPopup(new tt.Popup().setHTML("<b>Hello world!</b>"));
+class Marker {
+  constructor (nom, lat, lon, type, description, icone)
+  {
+    this.nom = nom;
+    this.lat = lat;
+    this.lon = lon;
+    this.type = type;
+    this.description = description;
+    this.icone = icone;
+  }
 
+  addMarqueur ()
+  {
+    var name = this.nom;
+    var divElement = document.createElement("div");
+    divElement.innerHTML =
+    '<div class="marker"><img src=" '+this.icone +'" width="32" height="40"><p>'+this.nom+'</p></div>';
+    var marker = new tt.Marker({
+         element: divElement,
+        })
+  .setLngLat([this.lat,this.lon])
+  .addTo(map)
+  .setPopup(new tt.Popup().setHTML("<b>" +this.description+ "</b>"));
+  }
+}
+
+
+
+//--------------------------------------ajout des marqueurs personnaliés---------------------------------------//
+
+//--------------------------------------------------IUT--------------------------------------//
+//cafet IUT
+
+ const cafetIUT = new Marker("Cafeteria Amphi Hubert Coudane", 2.170191469892004, 48.710711586415286, "alimentation", "Hello world", "./img/restaurant-icon.png"  );
+ cafetIUT.addMarqueur() 
+//--------------------------------------------------Henri-Moissan-----------------------------------------------//
 //cafet moissan
+const CafetMoissan = new Marker("Cafeteria Henri Moissan", 2.1723943695285683,48.711530079110744, "alimentation", "Hello world", "./img/restaurant-icon.png" )
 var divElement2 = document.createElement("div");
 divElement2.innerHTML =
   '<div class="marker"><img src="./img/restaurant-icon.png" width="32" height="40"><p>Cafeteria Henri Moissan</p></div>';
@@ -39,6 +65,38 @@ var marker2 = new tt.Marker({
   .addTo(map)
   .setPopup(new tt.Popup().setHTML("<b>Hello world!</b>"));
 
+
+//------------------------------------Polytech-Paris-Saclay--------------------------------//
+
+
+//--------------------------------------Lieu-de-vie-----------------------------------------//
+var divElement = document.createElement("div");
+divElement.innerHTML =
+  '<div class="marker"><img src="./img/restaurant-icon.png" width="32" height="40"><p>Cafetaria et restaurant universitaire du lieu de Vie</p></div>';
+var marker = new tt.Marker({
+  element: divElement,
+})
+  .setLngLat([2.171465539968687,48.709506800652576])
+  .addTo(map)
+  .setPopup(new tt.Popup().setHTML("<b>Hello world!</b>"));
+
+  var divElement = document.createElement("div");
+  divElement.innerHTML =
+    '<div class="marker"><img src="./img/restaurant-icon.png" width="32" height="40"><p>Fontaine d eau</p></div>';
+  var marker = new tt.Marker({
+    element: divElement,
+  })
+    .setLngLat([2.171443, 48.709453])
+    .addTo(map)
+    .setPopup(new tt.Popup().setHTML("<b>Hello world!</b>"));
+
+//--------------------------------------H-Bar---------------------------------------------------//
+
+//------------------------------------Elein-Gray---------------------------------------------//
+
+//-------------------------------------CS-Eiffel--------------------------------------------//
+
+
 map.on("zoomend", function () {
   if (map.getZoom() < 17.5) {
     marker.remove();
@@ -51,46 +109,77 @@ map.on("zoomend", function () {
   }
 });
 
-//tt.load().then(map);
-/*
-tt.setToken("Ue34QD5EuCWqslTHMvMzNIAhGwxEWhbj");
-tt.setProductInfo('MapsSDKExamples', '1.0');
-tt.services.setToken('Ue34QD5EuCWqslTHMvMzNIAhGwxEWhbj');
-var mapsService = tt.services.MapsService({ key: 'Ue34QD5EuCWqslTHMvMzNIAhGwxEWhbj' });
-var options = {
-  language: 'fr-FR',
-  minNumberOfCharacters: 2,
-  limit: 5
-};
-
-var searchBoxContainer = document.getElementById('searchBox');
-var ttSearchBox = new tt.plugins.SearchBox(mapsService, options)
-var searchMarkersManager = new SearchMarkersManager(map)
-ttSearchBox.on("tomtom.searchbox.resultsfound", handleResultsFound)
-ttSearchBox.on("tomtom.searchbox.resultselected", handleResultSelection)
-ttSearchBox.on("tomtom.searchbox.resultfocused", handleResultSelection)
-ttSearchBox.on("tomtom.searchbox.resultscleared", handleResultClearing)
-map.addControl(ttSearchBox, "top-right")
-searchBoxContainer.appendChild(ttsearchBox.getSearchBoxHTML());
-*/
 
 
+// créer une instance d'autocomplétion
+//var autocomplete = new H.places.Autocomplete(searchOptions);
+
+//création de la searchBox
  var searchOptions= {
       key: 'Ue34QD5EuCWqslTHMvMzNIAhGwxEWhbj',
       limit: 5,
       idxSet: 'POI,Geo',
-      language: 'fr-FR'
+      language: 'fr-FR',
+
+      autocomplete: {
+        enabled: true, // active l'autocomplétion
+        minLength: 2, // nombre minimum de caractères avant de lancer l'autocomplétion
+        debounceTime: 100, // délai avant d'envoyer une requête au serveur après la saisie de l'utilisateur
+        categories: [
+          {
+            filter: 'categorySet=POI',
+            searchIn: 'name',
+            label: 'Points d\'intérêt',
+          },
+          {
+            filter: 'categorySet=Address',
+            searchIn: 'address',
+            label: 'Adresses',
+          },
+        ],
+      },
+        
+        
   };
 
-/*
-var searchBox = new tt.plugins.SearchBox(tt.services, searchOptions);
-searchBox.addTo(map);*/
-
 var ttSearchBox = new tt.plugins.SearchBox(tt.services, searchOptions)
-/*var searchMarkersManager = new SearchMarkersManager(map)
-ttSearchBox.on("tomtom.searchbox.resultsfound", handleResultsFound)
-ttSearchBox.on("tomtom.searchbox.resultselected", handleResultSelection)
-ttSearchBox.on("tomtom.searchbox.resultfocused", handleResultSelection)
-ttSearchBox.on("tomtom.searchbox.resultscleared", handleResultClearing)*/
+
 map.addControl(ttSearchBox, "top-left")
+
+window.addEventListener('load', function() {
+  // Lorsqu'un utilisateur soumet une recherche
+  ttSearchBox.on('submit', function(event) {
+  // Empêche le formulaire de se recharger la page
+  event.preventDefault();
+  // Récupère la requête de recherche
+  var query = event.data;
+  
+  // Effectue une recherche géocodée pour récupérer les informations sur le lieu
+  tt.services.fuzzySearch({
+    key: 'Ue34QD5EuCWqslTHMvMzNIAhGwxEWhbj',
+    query: query,
+    language: 'fr-FR',
+    
+  })
+  .then(function(response) {
+    // Récupère les informations sur le lieu à partir de la réponse
+    var place = response.results[0];
+    
+    // Déplace la carte à la position du lieu
+    map.flyTo({
+      center: place.position
+    });
+    
+    // Ajoute un marqueur à la position du lieu
+    var marker = new tt.Marker().setLngLat(place.position).addTo(map);
+    
+    // Affiche les informations sur le lieu dans une fenêtre contextuelle
+    var popup = new tt.Popup({offset: 30}).setHTML('<h3>' + place.address.freeformAddress + '</h3><p>' + place.poi.name + '</p>');
+    marker.setPopup(popup).togglePopup();
+  })
+  .catch(function(error) {
+    console.error(error);
+  });
+});
+  });
 
