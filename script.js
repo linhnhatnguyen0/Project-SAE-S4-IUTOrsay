ca97b1f375b62aade244798518ca03be92afbe4c
linhnhatnguyen0 <51749625+linhnhatnguyen0@users.noreply.github.com>
2023-04-18 17:18:44 +0200

    Update calculateRoute

--- script.js
+++ script.js
@@ -465,3 +465,59 @@ window.addEventListener("load", function () {
       });
   });
 });
+
+var source = new tt.LngLat(2.1688376763699813, 48.7118157869293);
+var destination = new tt.LngLat(2.169020066581464, 48.709663607193576);
+
+var routeoptions = {
+  key: "Ue34QD5EuCWqslTHMvMzNIAhGwxEWhbj",
+  locations: [],
+  trafic: false,
+};
+
+routeoptions.locations.push(source);
+routeoptions.locations.push(destination);
+
+routeoptions.traffic = false;
+// routeoptions.travelMode = "pedestrian";
+
+const button = document.querySelector("#buttonRoute");
+button.addEventListener("click", clickHandler);
+
+function clickHandler() {
+  try {
+    tt.services.calculateRoute(routeoptions).then(function (response) {
+      var geojson = response.toGeoJson();
+      console.log(geojson);
+      displayRoute(geojson);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayRoute(geojson) {
+  map.addLayer({
+    id: "route",
+    type: "line",
+    source: {
+      type: "geojson",
+      data: geojson,
+    },
+    paint: {
+      "line-color": "#ff0000",
+      "line-width": 5,
+    },
+  });
+}
+
+tt.services
+  .fuzzySearch({
+    key: "Ue34QD5EuCWqslTHMvMzNIAhGwxEWhbj",
+    query: query,
+    language: "fr-FR",
+  })
+  .then(function (response) {
+    console.log(response);
+    // Do something with response
+  });
