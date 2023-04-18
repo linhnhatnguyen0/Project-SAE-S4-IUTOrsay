e12f42eb80e4f5a3be8b845f087a677fb1045765
linhnhatnguyen0 <51749625+linhnhatnguyen0@users.noreply.github.com>
2023-04-18 12:26:15 +0200

    update calculateRoute

--- script.js
+++ script.js
@@ -26,3 +26,56 @@ map.on("zoomend", function () {
     console.log("add");
   }
 });
+
+var source = new tt.LngLat(2.1688376763699813, 48.7118157869293);
+var destination = new tt.LngLat(2.169020066581464, 48.709663607193576);
+
+var routeoptions = {
+  key: "Ue34QD5EuCWqslTHMvMzNIAhGwxEWhbj",
+  locations: [],
+  trafic: false,
+};
+
+routeoptions.locations.push(source);
+routeoptions.locations.push(destination);
+
+routeoptions.traffic = false;
+routeoptions.travelMode = "pedestrian";
+const button = document.querySelector("#buttonRoute");
+button.addEventListener("click", clickHandler);
+
+function clickHandler() {
+  try {
+    tt.services.calculateRoute(routeoptions).then(function (response) {
+      var geojson = response.toGeoJson();
+      displayRoute(geojson);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayRoute(geojson) {
+  map.addLayer({
+    id: "route",
+    type: "line",
+    source: {
+      type: "geojson",
+      data: geojson,
+    },
+    paint: {
+      "line-color": "#ff0000",
+      "line-width": 5,
+    },
+  });
+}
+// var source = [2.1688376763699813, 48.7118157869293];
+// var destination = [2.169020066581464, 48.709663607193576];
+
+// map.addControl(new tt.FullscreenControl());
+// map.addControl(new tt.NavigationControl());
+
+// var markerSource = new tt.Marker().setLngLat(source).addTo(map);
+// var markerDestination = new tt.Marker().setLngLat(destination).addTo(map);
+
+// Replace 'your-api-key' with your actual TomTom API key
