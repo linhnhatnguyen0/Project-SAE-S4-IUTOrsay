4bfeddf064f4b3be434789b3739f94b8be605ffe
Sourine <103242820+Sourine@users.noreply.github.com>
2023-04-19 16:30:26 +0200

    derniére modif

--- MarkerFunctions.js
+++ MarkerFunctions.js
@@ -7,61 +7,30 @@ var map = tt.map({
 
   const jsonFilePath = "markerData.json";
 
-  map.on('load', function() {
-    addMarkersToMap(jsonFilePath);
-});
-
 //---------------------------- Ajouter les markers a la map -------------------------//
 
-  
-function addMarkersToMap(jsonFilePath) {
+
+  function addMarkersToMap(jsonFilePath) {
     fetch(jsonFilePath)
       .then(response => response.json())
-      .then(markerData => {
-        const markers = markerData.markerData.map(marker => {
-          const ttMarker = new tt.Marker({
-            element: document.createElement('div'),
-            anchor: 'bottom',
-            draggable: false,
-            icon: {
-              url: marker.icon,
-              size: [50, 50],
-              anchor: 'bottom',
-            },
-          })
-          .setLngLat([marker.lat, marker.lng])
-          .addTo(map);
-  
-          // Ajouter un événement de clic pour afficher une info-bulle avec les détails du marqueur
-          ttMarker.on('click', () => {
-            const popup = new tt.Popup({ offset: 30 }).setHTML(`
-              <p>${marker.nom}</p>
-              <p>${marker.description}</p>
-            `);
-            ttMarker.setPopup(popup).togglePopup();
+      .then(data => {
+          data.markerData.forEach(marker => {
+            var divElement = document.createElement("div");
+            divElement.innerHTML =
+            '<div class="marker"><img src=" ' + marker.icon + '" width="22" height="26"></div>'
+            const ttMarker = new tt.Marker({ 
+              element: divElement,
+            })
+              .setLngLat([marker.lng, marker.lat])
+              .setPopup(new tt.Popup().setHTML("<b>" + marker.description + "</b>"))
+              .addTo(map);
           });
-        });
-      })
-      .catch(err => {
-        console.error(err);
       });
   }
 
-  addMarqueur() {
-    var divElement = document.createElement("div");
-    divElement.innerHTML =
-      '<div class="marker"><img src=" ' +
-      this.icone +
-      '" width="22" height="26"></div>';
-    var marker = new tt.Marker({
-      element: divElement,
-    })
-      .setLngLat([this.lat, this.lon])
-      .addTo(map)
-      .setPopup(new tt.Popup().setHTML("<b>" + this.description + "</b>"));
-    var lat = this.lat;
-    var lon = this.lon;
-    divElement.addEventListener("click", markerHandler(lat, lon));
-  }
+  addMarkersToMap(jsonFilePath);
+  
+  
+
   
   
