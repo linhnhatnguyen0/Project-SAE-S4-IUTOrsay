45f7fda9692e0a29128adaf14734bb8d8a6dba82
NGUYEN Nhat Linh <51749625+linhnhatnguyen0@users.noreply.github.com>
2023-04-20 12:02:40 +0200

    Update css button

--- MarkerFunctions.js
+++ MarkerFunctions.js
@@ -1,9 +1,4 @@
-var map = tt.map({
-  key: "Ue34QD5EuCWqslTHMvMzNIAhGwxEWhbj",
-  container: "map",
-});
-map.setZoom(17);
-map.setCenter([2.170936, 48.711115]);
+import { map } from "./mapDisplay.js";
 
 const markerData = [
   {
